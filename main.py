1099b33dca33fca0428a4f7ceea1c75bc3520f38
Aleksandr Popach <braindigger89@gmail.com>
2022-04-21 18:53:10 +0300

    Did first tests to understand how Telegram Bot API works. Decided to use raw API without additional Python libraries

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import requests
+
+token = '5398687279:AAF4t4akIuMjo2Zmrmd2GBX5SGAOg-Ot6as'
+params = {
+    'offset': 0,
+    'timeout': 2
+}
+
+while True:
+    response = requests.post(f'https://api.telegram.org/bot{token}/getUpdates', params=params)
+    updates = response.json()
+    if not updates['result']:
+        print('No messages')
+    else:
+        for new_message in updates['result']:
+            print(new_message['message']['text'])
+        last_id = updates['result'][-1]['update_id']
+        params['offset'] = last_id + 1
+        if updates['result'][-1]['message']['text'] == 'stop':
+            break
+
+
 
