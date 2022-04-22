caec7c6d28f6617ace51434e20b586cd630d275b
Aleksandr Popach <braindigger89@gmail.com>
2022-04-23 00:04:11 +0300

    Started working on the bot's greeting function.

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
 import requests
 
 token = '5398687279:AAF4t4akIuMjo2Zmrmd2GBX5SGAOg-Ot6as'
-params = {
-    'offset': 0,
-    'timeout': 2
-}
+handlers = {'/start': 'start_handler', '/restart': 'start_handler'}
 
-while True:
-    response = requests.post(f'https://api.telegram.org/bot{token}/getUpdates', params=params)
-    updates = response.json()
-    if not updates['result']:
-        print('No messages')
-    else:
-        for new_message in updates['result']:
-            print(new_message['message']['text'])
-        last_id = updates['result'][-1]['update_id']
-        params['offset'] = last_id + 1
-        if updates['result'][-1]['message']['text'] == 'stop':
-            break
+
+def start_handler():
+    pass
+
+
+def get_updates():
+    params = {
+        'offset': 0,
+        'timeout': 2
+    }
+    while True:
+        response = requests.post(f'https://api.telegram.org/bot{token}/getUpdates', params=params)
+        updates = response.json()
+        if updates['result']:
+            for new_message in updates['result']:
+                print(new_message)
+                text = new_message['message']['text']
+                if text in handlers:
+                    handler_to_call = handlers[text]
+                    globals()[handler_to_call]()
+                else:
+                    print('Выдаём список доступных команд пользователю')
+            last_id = updates['result'][-1]['update_id']
+            params['offset'] = last_id + 1
+
+get_updates()
 
 
 
